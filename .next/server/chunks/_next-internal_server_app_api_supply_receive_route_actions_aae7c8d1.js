a4951c360382cb30457ea991d5ffb38abef3f203
Miguel Perdomo <filimorniga@GMAIL.COM>
2025-11-29 14:22:42 -0300

    FIX: Ignorar carpeta de build local

--- .next/server/chunks/_next-internal_server_app_api_supply_receive_route_actions_aae7c8d1.js
+++
@@ -1,3 +0,0 @@
-module.exports=[97316,s=>{"use strict";s.s([])}];
-
-//# sourceMappingURL=_next-internal_server_app_api_supply_receive_route_actions_aae7c8d1.js.map
--- .next/server/chunks/ssr/_next-internal_server_app_finanzas_page_actions_66e83224.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33793,a=>{"use strict";a.s([])}];
-
-//# sourceMappingURL=_next-internal_server_app_finanzas_page_actions_66e83224.js.map
